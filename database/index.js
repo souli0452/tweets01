0fdf2be929b73bb37093fb38d7db23379ddfa8cc
SOULI Issouf <souliissouf123@gmail.com>
2022-11-17 17:17:07 +0000

    1er de twtter01

---
+++ database/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(
+    'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log('connexion db ok !'))
+  .catch((err) => console.log(err));
